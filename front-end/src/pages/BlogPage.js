9cfe8d362d42aa11699ce32d1305115e157bf62b
IzamaraJordao <89167184+IzamaraJordao@users.noreply.github.com>
2022-11-08 21:07:57 -0300

    alteração do favicon

--- front-end/src/pages/BlogPage.js
+++ front-end/src/pages/BlogPage.js
@@ -21,7 +21,7 @@ export default function BlogPage() {
   return (
     <>
       <Helmet>
-        <title> Dashboard: Blog | Minimal UI </title>
+        <title>Apply</title>
       </Helmet>
 
       <Container>
