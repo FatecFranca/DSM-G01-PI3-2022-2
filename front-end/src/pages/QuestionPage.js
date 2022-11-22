aec60904c5f2307fc92ad1bc0e5e14293c254f04
math3us2021 <matheusbf.mf@gmail.com>
2022-11-22 20:27:14 -0300

    feat: tela questionario

---
+++ front-end/src/pages/QuestionPage.js
@@ -0,0 +1,11 @@
+import { dividerClasses } from '@mui/material'
+import React from 'react'
+import Accordion from '../components/accordeon'
+
+export default function QuestionPage() {
+  return (
+    <>
+    <Accordion/>
+    </>
+  )
+}
