99935cb910ef0b3bfea7997dd0e0113b8e6bfae3
Arthur Carrenho <arthur.carrenho@unifacef.com.br>
2022-11-23 17:54:59 -0300

    router

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -23,7 +23,7 @@ export default function Router() {
         { path: 'app', element: <DashboardAppPage /> },
         { path: 'user', element: <UserPage /> },
         { path: 'products', element: <ProductsPage /> },
-        { path: 'blog', element: <BlogPage /> },
+        { path: 'glossary', element: <BlogPage /> },
       ],
     },
     {
